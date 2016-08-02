4cc7a0e672c3b170b2dfe46e7284c428375cd0e9
Samantha <Samantha@Mine>
2016-08-02 14:39:37 -0500

    whenCoinIsInsertedScreenIsUpdated

--- src/vendingMachine/VendingMachine.java
+++ src/vendingMachine/VendingMachine.java
@@ -1,5 +1,30 @@
 package vendingMachine;
 
+import java.math.BigDecimal;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class VendingMachine {
+	private BigDecimal currentAmount = new BigDecimal(0);
+	NumberFormat n = NumberFormat.getCurrencyInstance(Locale.US);
+	private String display = "";
+
+	public void insertCoin(String coin) {
+			coin = coin.toLowerCase();
+			switch(coin){
+			case("nickel"):
+				currentAmount = currentAmount.add(new BigDecimal(.05));
+				break;
+			}
+			setDisplay(n.format(currentAmount));
+	}
+	
+	public String getDisplay() {
+		return display;
+	}
+
+	public void setDisplay(String display) {
+		this.display = display;
+	}
 
 }
--- src/vendingMachineTest/VendingMachineTest.java
+++ src/vendingMachineTest/VendingMachineTest.java
@@ -1,12 +1,18 @@
 package vendingMachineTest;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
+import vendingMachine.VendingMachine;
 
 public class VendingMachineTest {
 	
-@Test
-public void test(){
+	@Test
+	public void whenCoinIsInsertedScreenIsUpdated(){
+		VendingMachine vm = new VendingMachine(); 
+		vm.insertCoin("Nickel");
+		String screen = vm.getDisplay();
+		assertEquals("$0.05", screen );
+	}
 	
-}
-
 }
